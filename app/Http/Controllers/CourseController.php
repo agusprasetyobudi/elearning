c324293462ba3e0a2df8be113ffddfa8beaf613a
agus prasetyo <agus.prasetyobudi@gmail.com>
2020-01-26 22:37:54 +0700

    Inserting List Courses

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -163,6 +163,7 @@ class CourseController extends Controller
         $company_name = SettingApplication::where('id',1)->get();
         $role = Auth::user();
         $name_user = $role->name;
+        $list_course = CourseModels::where('parent_id',$id)->get();
         $course_title = CourseModels::where([['parent_id','=', 0],['status','=', 1]])->get();
         $detail = CourseModels::where([['id',$id],['status',1]])->first();
         if(!empty($detail)){
