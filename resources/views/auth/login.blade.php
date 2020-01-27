f6c3aaf5fc564a3ccd1eb322168d85ab103fdd46
agus prasetyo <agus.prasetyobudi@gmail.com>
2020-01-28 00:02:52 +0700

    update sementara

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -99,11 +99,11 @@
       <!-- /.social-auth-links -->
 
       <p class="mb-1">
-        <a href="{{route('password.request')}}">I forgot my password</a>
+        <a href="#">I forgot my password</a>
         {{-- <a href="forgot-password.html">I forgot my password</a> --}}
       </p>
       <p class="mb-0">
-        <a href="{{route('register')}}" class="text-center">Register a new membership</a>
+        <a href="#" class="text-center">Register a new membership</a>
         {{-- <a href="register.html" class="text-center">Register a new membership</a> --}}
       </p>
     </div>
