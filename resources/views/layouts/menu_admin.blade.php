a36a4f7f2a7d49500fdb7d40f2b0c20c29f16bb6
agus prasetyo <agus.prasetyobudi@gmail.com>
2020-02-06 01:21:13 +0700

    update logo side bar company

--- resources/views/layouts/menu_admin.blade.php
+++ resources/views/layouts/menu_admin.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{!! route('DashboardAdmin') !!}" class="brand-link">
-    <img src="{{asset('admin/images/LogoCompany.jpeg')}}"   alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <img src="{{asset('admin/images/CompanyLogos.png')}}"   alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
          style="opacity: .8">
     @if ($company_name)
         @foreach ($company_name as $item)
